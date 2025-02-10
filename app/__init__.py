55403fb9f4f886d8cc810c3ac07c026afd238c69
manual-dos-bixos <manualdosbixos.fatec@gmail.com>
2025-02-10 13:48:53 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ def create_app():
     # Configurações do app
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres.otehdyoibzmgrqufxbgm:m4BS9EOVC3NsiVzM@aws-0-sa-east-1.pooler.supabase.com:5432/postgres'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = os.environ.get('secret_key')
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
     # Inicializa o SQLAlchemy com o app
     db.init_app(app)
